5ab5e5b1793dc88689901c7531c093872c810f8a
Barranger Ridler <barranger@redbitdev.com>
2018-07-10 21:43:04 -0400

    General Cleanup

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -64,7 +64,7 @@ const Sidebar = ({ ...props }) => {
   );
   var brand = (
     <div className={classes.logo}>
-      <a href="#" className={classes.logoLink}>
+      <a href="/my-pic-stats" className={classes.logoLink}>
         <div className={classes.logoImage}>
           <img src={logo} alt="logo" className={classes.img} />
         </div>
